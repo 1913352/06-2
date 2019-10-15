c2f00a60869e6d6f8823aa666510b63ea9130ecc
1913352 <55537201+1913352@users.noreply.github.com>
2019-10-15 15:39:54 +0900

    practice2

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(void)
+{
+	int i;
+	int max = 45;
+	
+	srand((unsigned)time(NULL));
+	for(i=0; i<6; i++)
+	printf("%d ", 1+rand()%max);
+	
+	return 0;
+}
